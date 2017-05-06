3dd6592fa769f93b6118cf20a4e43dd6082e835b
maxfomin <maxf177@gmail.com>
2017-05-06 16:31:02 +0300

    Added matrix regression file with empty model

---
+++ matrix_regression.py
@@ -0,0 +1,46 @@
+import numpy as np
+import tensorflow as tf
+conf = {
+    'sequence_len': 3,
+    'properties_number': 20,
+    'hidden_size': 4,
+    'label_distance': 5
+}
+
+class Model:
+
+    def __init__(self, data, target):
+        self.data = data
+        self.target = target
+        self._prediction = None
+        self._optimize = None
+        self._error = None
+
+    @property
+    def prediction(self):
+        if not self._prediction:
+            data_size = int(self.data.get_shape()[1])
+            target_size = int(self.target.get_shape()[1])
+            # weight = tf.Variable(tf.truncated_normal([data_size, target_size]))
+            # bias = tf.Variable(tf.constant(0.1, shape=[target_size]))
+            # incoming = tf.matmul(self.data, weight) + bias
+            # self._prediction = tf.nn.softmax(incoming)
+        return self._prediction
+
+    @property
+    def optimize(self):
+        if not self._optimize:
+            cross_entropy = -tf.reduce_sum(self.target, tf.log(self.prediction))
+            optimizer = tf.train.RMSPropOptimizer(0.03)
+            self._optimize = optimizer.minimize(cross_entropy)
+        return self._optimize
+
+    @property
+    def error(self):
+        if not self._error:
+            mistakes = tf.not_equal(
+                tf.argmax(self.target, 1), tf.argmax(self.prediction, 1))
+            self._error = tf.reduce_mean(tf.cast(mistakes, tf.float32))
+        return self._error
+
+
